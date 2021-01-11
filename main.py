c945ff17b4f7a87f793ad8279e755f51b17c2492
Cooper Wainczak <gainczak@gmail.com>
2021-01-11 00:18:24 -0500

    Added Menu, Improved Searcher, etc
    - Added new Main Menu with options
    - Added a backup case for when the no videos are found
    - Now keeps track of what videos lead to no videos found
    - Added functionality to determine what the missing videos are
    - Organized and cleaned-up code
    
    TODO:
    - Complete missing videos function; add functionality for multiple .html files

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import webbrowser
 import pyautogui
 import time
+import re
 
 def main():
+    choice = getOption()
+    if choice == "1":
+        # parse video titles
+        parsePlaylistForVideos()
+    elif choice == "2":
+        # check for missing videos
+        print("These are the missing videos:\n")
+        for title in checkForMissingVideos():
+            print(title)
+    elif choice == "3":
+        # continue publishing
+        continuePublishing()
+    elif choice == "4":
+        exit(1)
+
+def continuePublishing():
     staffName = input("Enter the staff's last name: ")
+    # get amount of titles
+    videosLeft = getLineCount("video_titles.txt")
     # open pipeline
     webbrowser.open("https://pipeline.westfield.ma.edu/app/library.aspx")
     wait(4)
@@ -14,13 +33,36 @@ def main():
     line = f.readline()
     while line != "":
         line = dealWithExtraCharacters(line)
-        publishVideo(line, staffName)
-        doneMessage = line + "\t|\tDONE"
-        print(doneMessage)
+        # if it failed, note and refresh
+        if publishVideo(line, staffName) is False:
+            doneMessage = "\nThe video \"" + line + "\" has not been published"
+            print(doneMessage)
+        else:
+            videosLeft -= 1
+            doneMessage = line + "\t|\tDONE"
+            print(doneMessage + "\t(" + str(videosLeft) + " more!)")
         p.write(doneMessage + "\n")
+        p.flush()
         line = f.readline()
     p.close()
     f.close()
+
+def getOption():
+    choice = input("--------------------------------------------------------\n"
+                   "Parse playlist .html file for video titles: 1\n"
+                   "Check for shared library missing videos: 2\n"
+                   "Continue publishing from current progress: 3\n"
+                   "Exit: 4\n"
+                   "--------------------------------------------------------\n"
+                   "Select an option - ")
+    while choice != "1" and choice != "2" and choice != "3" and choice != "4":
+        print("Invalid input. Enter respective number to continue.")
+        choice = input("Select an option -\n"
+                       "Parse current .html file for video titles: 1\n"
+                       "Check for shared library missing videos: 2\n"
+                       "Continue publishing from current progress: 3\n"
+                       "Exit: 4")
+    return choice
 
 def dealWithExtraCharacters(title):
     if "," in title:
@@ -36,9 +78,6 @@ def dealWithExtraCharacters(title):
         title = title.split("\n")[0]
     return title
 
-def deleteLine():
-    o = open("video_titles.txt")
-
 def getLongerPartOfString(titleList):
     # longer side is generally more descriptive, so it is useful to return the longer side
     longest = ""
@@ -46,6 +85,70 @@ def getLongerPartOfString(titleList):
         if len(part) > len(longest):
             longest = part
     return longest
+
+def parsePlaylistForVideos():
+    dir = "full playlists/" + input("Enter the staff's last name: ") + ".html"
+    #rule = r"<div class=\"ev-pl-item-title\" title=\"([a-zA-Z0-9 '?&{},#;$\.\-/\(\)]*)"
+    rule = r"<div class=\"ev-pl-item-title\" title=\"([^<>\"]*)\""
+    v = open(dir, "r")
+    data = v.read().replace('\n', '').replace('\r', '')
+    v.close()
+    titles = re.findall(rule, data, re.IGNORECASE)
+    # write titles to video_titles.txt
+    vt = open("all_video_titles.txt", "w")
+    for title in titles:
+        if title != "${name}":
+            if "&#39;" in title: # html code for '
+                title = title.replace("&#39;", "'")
+            if "&amp;" in title: # html code for &
+                title = title.replace("&amp;", "&")
+            if "&quot;" in title: # html code for "
+                title = title.replace("&quot;", '"')
+            vt.write(title + "\n")
+    vt.close()
+
+def checkForMissingVideos():
+    # TAKE A LOOK AT THIS TO MAKE SURE IT WORKS PROPERLY
+
+    # add all videos to list
+    expectedVideoList = []
+    v = open("all_video_titles.txt", "r")
+    line = v.readline().replace('\n', '')
+    while line != "":
+        expectedVideoList.append(line)
+        line = v.readline().replace('\n', '')
+
+    # get titles from shared library
+    v = open("D:/Downloads/raw_html.html", "r")
+    data = v.read().replace('\n', '').replace('\r', '')
+    rule = r"target=\"_blank\" title=\"Preview: ([^<>\"]*)\""
+    v.close()
+    actualVideoList = re.findall(rule, data, re.IGNORECASE)
+
+    # compare and keep track of missing videos
+    for i in range(len(actualVideoList)):
+        actualVideoList[i] = actualVideoList[i].lower()
+
+    missingVideos = []
+    for title in expectedVideoList:
+        if title.lower() not in actualVideoList:
+            # add to missing videos
+            missingVideos.append(title)
+    return missingVideos
+
+def getLineCount(textFile):
+    lineCount = 0
+    f = open(textFile, "r")
+    line = f.readline()
+    while line != "":
+        lineCount += 1
+        line = f.readline()
+    return lineCount
+
+def isFailedSearch():
+    if pyautogui.locateOnScreen("images/no_items_found.png") is None:
+        return False
+    return True
 
 def publishVideo(title, staffName):
 
@@ -55,6 +158,10 @@ def publishVideo(title, staffName):
     pyautogui.hotkey("enter")
     # wait for load time
     wait(4)
+
+    # if search fails, return false
+    if isFailedSearch():
+        return False
 
     # moves to publish
     pyautogui.moveTo(1723, 741)
@@ -108,6 +215,9 @@ def publishVideo(title, staffName):
     # click on publish
     pyautogui.leftClick()
     wait(5)
+
+    # if it worked
+    return True
 
 def multiLeftClicker(numClicks):
     for x in range(numClicks):
